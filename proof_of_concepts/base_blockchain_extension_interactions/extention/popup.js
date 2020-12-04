bda37418ab452bd1d77219b0f9b580aa99ebe19e
Nico Hohm <nico.hohm@yahoo.com>
2020-12-04 20:53:04 +0100

    button navigation and pages

--- proof_of_concepts/base_blockchain_extension_interactions/extention/popup.js
+++ proof_of_concepts/base_blockchain_extension_interactions/extention/popup.js
@@ -266,6 +266,30 @@
             });
         });
     
-        function storageSave(value, key){
-            chrome.storage.sync.set({ key: value });
+/* -------------------------------------------------------------------------------------------
+        *                                   Menu Navigation
+        ------------------------------------------------------------------------------------------- */
+        var cardArray=[
+        document.getElementById("menuCard"),
+        document.getElementById("pollsCard"),
+        document.getElementById("repoCard"),
+        document.getElementById("pullCard"),
+        document.getElementById("walletCard")
+        ]
+        document.getElementById("gotoRepoBtn").addEventListener("click",function(){gotoCard(2)});
+        document.getElementById("gotoWalletBtn").addEventListener("click",function(){gotoCard(4)});
+        document.getElementById("gotoPollsBtn").addEventListener("click",function(){gotoCard(1)});
+        document.getElementById("gotoPullBtn").addEventListener("click",function(){gotoCard(3)});
+        var backBtns = document.getElementsByClassName("backBtn");
+        for (let i = 0; i < backBtns.length; i++) {
+            backBtns[i].addEventListener("click",function(){gotoCard(0)});
+        }
+        
+
+        function gotoCard(index){
+            console.log("change card");
+            cardArray.forEach(element => {
+                element.style.display="none";
+            });
+            cardArray[index].style.display="block";
         }
